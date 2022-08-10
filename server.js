8491bab125df11e96a52fedc1224693c389771c3
Jacob Wilkinson <95652243+wilkija@users.noreply.github.com>
2022-08-09 20:20:50 -0500

    comments to server.js

--- server.js
+++ server.js
@@ -21,25 +21,25 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 
-app.get('/',async (request, response)=>{
+app.get('/',async (request, response)=>{ // GET request under root that is used to display the todo list on the app's main page
     const todoItems = await db.collection('todos').find().toArray()
     const itemsLeft = await db.collection('todos').countDocuments({completed: false})
     response.render('index.ejs', { items: todoItems, left: itemsLeft })
-    // db.collection('todos').find().toArray()
-    // .then(data => {
-    //     db.collection('todos').countDocuments({completed: false})
-    //     .then(itemsLeft => {
-    //         response.render('index.ejs', { items: data, left: itemsLeft })
-    //     })
-    // })
-    // .catch(error => console.error(error))
+    db.collection('todos').find().toArray() // Go to MongoDB collection named 'todos', find all of the documents (objects), and put them into an array
+    .then(data => { // From the returned promise, pass the array into the variable data
+        db.collection('todos').countDocuments({completed: false}) // Determine the documents from the todo that are marked 'incomplete'
+        .then(itemsLeft => {
+            response.render('index.ejs', { items: data, left: itemsLeft }) // Send a response by rendering the ejs with the data under the name 'items'
+        })
+    })
+    .catch(error => console.error(error))
 })
 
-app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
-    .then(result => {
-        console.log('Todo Added')
-        response.redirect('/')
+app.post('/addTodo', (request, response) => { // POST request under 'addTodo' route. Used to add a new todo item to the list
+    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false}) // Access the 'todos' collection in MongoDB, insert an item with the title recieved from the request (thing from the form in ejs) and the additional property of incomplete
+    .then(result => { // After promise is recieved
+        console.log('Todo Added') // Affirm that the todo item was added in the console
+        response.redirect('/') // Refresh the page to see the changes of the create response
     })
     .catch(error => console.error(error))
 })
